cc9009a870957c9043ac9ea41d69654dc3ad319e
Siddesh <sidsiddesh11@gmail.com>
2025-09-01 10:53:26 +0530

    first commit

---
+++ src/main/java/com/project/base/Basetest.java
@@ -0,0 +1,49 @@
+package com.project.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.project.utilities.ExtentManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basetest {
+	protected WebDriver driver;
+	protected ExtentReports extent;
+	protected ExtentTest test;
+	@BeforeSuite
+	public void setupreport()
+	{
+		extent=ExtentManager.getinstance();
+	}
+	
+	
+	@BeforeMethod
+	  public void setup() {
+		  System.out.println("Before method");
+		  WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  }
+	  @AfterMethod
+	  public void teardown() {
+		  System.out.println("After method");
+		  driver.quit();
+	  }
+	
+	@AfterSuite
+public void flushreport()
+
+{
+	extent.flush();
+}
+}
