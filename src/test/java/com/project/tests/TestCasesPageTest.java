a00696d4c42f1a7cef493e1c850dddccf0631328
vivekpatilcgi <vivekpatil280803@gmail.com>
2025-09-12 10:57:50 +0530

    Update TestCasesPageTest.java

--- src/test/java/com/project/tests/TestCasesPageTest.java
+++ src/test/java/com/project/tests/TestCasesPageTest.java
@@ -1,15 +1,12 @@
 package com.project.tests;
 
+import com.aventstack.extentreports.Status;
 import com.project.base.Basetest;
 import com.project.pages.TestCasesPage;
-import com.project.utilities.ScreenshotUtilities;   // ✅ added
-import com.aventstack.extentreports.Status;        // ✅ added
-
+import com.project.utilities.ScreenshotUtilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import java.io.IOException;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
 
 public class TestCasesPageTest extends Basetest {
 
@@ -18,26 +15,17 @@ public class TestCasesPageTest extends Basetest {
     @BeforeMethod
     public void setupPage() {
         driver.get("https://www.automationexercise.com/test_cases");
-
         testCasesPage = new TestCasesPage(driver);
-        testCasesPage.initTestCaseLocators();
-
+        testCasesPage.initTestCaseLocators(); 
         test = extent.createTest("TC_ECOM_TestCases - Verify Clicking All Test Cases");
     }
 
     @Test
     public void testClickAllTestCases() throws IOException {
-        try {
-            testCasesPage.clickAllTestCases();
-            test.log(Status.PASS, "Clicked all test cases successfully")
-                .addScreenCaptureFromPath(ScreenshotUtilities.Capture(driver, "TestCasesPage"));
-        } catch (Exception e) {
-            test.log(Status.FAIL, "Failed to click all test cases: " + e.getMessage())
-                .addScreenCaptureFromPath(ScreenshotUtilities.Capture(driver, "TestCasesPageFail"));
-            throw e;
-        }
+        test.log(Status.INFO, "Starting the process of clicking all test cases.");
+        testCasesPage.clickAllTestCases(test);
+
+        test.log(Status.INFO, "Process complete. Final page state captured.");
+        test.addScreenCaptureFromPath(ScreenshotUtilities.Capture(driver, "TestCasesPage_Final"));
     }
-
-
-
 }
